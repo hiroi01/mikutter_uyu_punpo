56dfbca5ab886d05b23d7deb54b64dd985e648b5
hiroi01 <hiroi01.com@gmail.com>
2013-05-23 00:25:46 +0900

    add uyu_punpo_postbox command
    ぅゅぷんぽすとぼっくすコマンドを追加

--- uyu_punpo.rb
+++ uyu_punpo.rb
@@ -10,7 +10,16 @@ Plugin.create :uyu_punpo do
 			opt.messages.each { |m|
 				Service.primary.post(:message=>"@#{m.user[:idname]} (´･ェ･｀)「あ、もしもし#{m.user[:name]}さん？ぅゅだけど…うん、そうぷんぽぷんぽ」",:replyto=>m)
 			}
-	end
+		end
+
+	command(:uyu_punpo_postbox,
+		name: '(´･ェ･｀)ぅゅぷんぽすとぼっくす',
+		condition: lambda{ |opt| true },
+		visible: true,
+		role: :postbox) do |opt|
+			text = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
+		    Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = "(´･ェ･｀)「あ、もしもし#{text}さん？ぅゅだけど…うん、そうぷんぽぷんぽ」"
+		end
 
 end
 
